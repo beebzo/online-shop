2f3ab6d647ac7e11d9bc49b2fd8fc92606c6a55c
beebzo <bahatulen@gmail.com>
2020-12-05 12:52:58 +0300

    Filter Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import React from "react"
 import data from './data.json'
 import Products from './components/Products'
+import Filter from "./components/Filter";
 
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
             products:data.products,
-            size: '',
+            options: '',
             sort: ''
+        }
+    }
+    sortProducts = (event) => {
+        //impl
+        const sort = event.target.value;
+        console.log(event.target.value)
+        this.setState(() => ({
+            sort: sort,
+            products: this.state.products
+                .slice()
+                .sort((a,b) =>
+                sort === 'lowest'
+                    ? a.price > b.price
+                    ? 1
+                    :-1
+                    : sort === 'highest'
+                    ? a.price < b.price
+                        ? 1
+                        :-1
+                    : a._id > b._id
+                        ? 1
+                        :-1
+                ),
+        }))
+    }
+    filterProducts = (event) => {
+        //impl
+        console.log(event.target.value);
+        if (event.target.value === '') {
+            this.setState({options: event.target.value, product: data.products});
+        } else {
+            this.setState({
+                options: event.target.value,
+                products: data.products.filter
+                (product => product.availableOptions.indexOf(event.target.value) >= 0
+                ),
+            })
         }
     }
     render() {
@@ -20,6 +58,12 @@ class App extends React.Component {
       <main>
           <div className='content'>
               <div className='main'>
+                  <Filter
+                      count={this.state.products.length}
+                      options={this.state.options}
+                      sort={this.state.sort}
+                      filterProducts={this.filterProducts}
+                      sortProducts={this.sortProducts}  />
                   <Products products={this.state.products} />
               </div>
           <div className='sidebar'>Корзина</div>
---
+++ src/components/Filter.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+
+class Filter extends Component {
+    render() {
+        return (
+            <div className='filter'>
+                <div className='filter-result'>{this.props.count} Товары</div>
+                <div className='filter-sort'>
+                    Сортировать{' '}
+                    <select value={this.props.sort} onChange={this.props.sortProducts}>
+                        <option>Новинки</option>
+                        <option value="lowest">Дешевле</option>
+                        <option value="highest">Дороже</option>
+                    </select>
+                </div>
+                <div className='filter-options'>
+                    Фильтр {' '}
+                    <select value={this.props.options} onChange={this.props.filterProducts}>
+                        <option value=''>Все</option>
+                        <option value='32gb'>32gb</option>
+                        <option value='64gb'>64gb</option>
+                        <option value='128gb'>128gb</option>
+                        <option value='256gb'>256gb</option>
+                        <option value=''>Другое</option>
+                    </select>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Filter;
