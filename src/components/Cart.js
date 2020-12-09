10e05a1832c7b8a3251d3350ae7424ccdf4465a0
beebzo <bahatulen@gmail.com>
2020-12-09 20:38:07 +0300

    Checkout form

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,6 +2,28 @@ import React, {Component} from 'react';
 import formatCurrency from "../util";
 
 class Cart extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name:'',
+            email:'',
+            address:'',
+            showCheckout: false
+        }
+    }
+    handleInput = (e) => {
+    this.state({[e.target.name]: e.target.value})
+    }
+    createOrder = (e) => {
+        e.preventDefault();
+        const order = {
+            name: this.state.name,
+            email: this.state.email,
+            address: this.state.address,
+            cartItems: this.props.cartItems
+    }
+    this.props.createOrder(order)
+}
     render() {
         const {cartItems} = this.props;
         return (
@@ -9,22 +31,26 @@ class Cart extends Component {
                 {cartItems.length === 0? (
                     <div className='cart cart-header'>В корзине ничего нет</div>
                     )  :  (
-                    <div className='cart cart-header'>В вашей корзине {cartItems.length} товар(ов){' '}</div>
+                    <div className='cart cart-header'>
+                        В вашей корзине {cartItems.length} товар(ов){' '}
+                    </div>
                 )}
                 <div>
-                    <div className='cart'>
-                        <ul className='cart-items'>
-                            {cartItems.map (item =>(
-                                <li key={item.id}>
+                    <div className="cart">
+                        <ul className="cart-items">
+                            {cartItems.map((item) => (
+                                <li key={item._id}>
                                     <div>
-                                        <img src={item.image} alt={cartItems.title} />
+                                        <img src={item.image} alt={item.title} />
                                     </div>
                                     <div>
                                         <div>{item.title}</div>
-                                        <div className='right'>
-                                            {formatCurrency (item.price)} x {item.count} {' '}
-                                            <button className='button' onClick={() => this.props.removeFromCart(item)}>
-                                                Убрать
+                                        <div className="right">
+                                            {formatCurrency(item.price)} x {item.count}{" "}
+                                            <button
+                                                className="button"
+                                                onClick={() => this.props.removeFromCart(item)}>
+                                                Remove
                                             </button>
                                         </div>
                                     </div>
@@ -33,18 +59,55 @@ class Cart extends Component {
                         </ul>
                     </div>
                     {cartItems.length!==0 && (
-                        <div className='total'>
-                            <div>
-                                Итого: {' '}
-                                {formatCurrency(
-                                    cartItems.reduce((a, c) => a + c.price*c.count, 0)
-                                )}
+                        <div>
+                            <div className='cart'>>
+                                <div className='total'>
+                                    <div>
+                                     Итого: {' '}
+                                     {formatCurrency(
+                                        cartItems.reduce((a, c) => a + c.price*c.count, 0)
+                                     )}
                             </div>
-                            <button className='button primary'>Proceed</button>
+                                <button onClick={()=>{this.setState({showCheckout: true})}}
+                                        className='button primary'>
+                                    Продолжить
+                                </button>
+                            </div>
                         </div>
-
+                    {this.state.showCheckout && (
+                        <div className='cart'>
+                          <form onSubmit={this.createOrder}>
+                            <ul className="form-container">
+                              <li>
+                                <label>E-mail</label>
+                                <input name='email'
+                                  type='email'
+                                  required onChange={this.handleInput}>
+                                </input>
+                              </li>
+                              <li>
+                                 <label>Имя</label>
+                                    <input name='name'
+                                      type='text'
+                                      required onChange={this.handleInput}>
+                                    </input>
+                              </li>
+                               <li>
+                                 <label>Адрес</label>
+                                 <input  name="address"
+                                    type='text'
+                                    required onChange={this.handleInput}>
+                                 </input>
+                               </li>
+                               <li>
+                                  <button classname='button primary' type='submit'>Отправить</button>
+                               </li>
+                            </ul>
+                        </form>
+                        </div>
+                        )}
+                      </div>
                     )}
-
                 </div>
             </div>
         );
